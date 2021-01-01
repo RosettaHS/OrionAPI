eff1ad2764591185852d60500c56a4a5df9c5ead
Mark Redmann <markredmann@icloud.com>
2021-01-02 05:35:59 +0800

    Added base files

---
+++ src/include/errdef.h
@@ -0,0 +1,44 @@
+/*
+	Copyright (c) 2020 Rosetta H&S International.
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
+#ifndef __ORION_OKIT_ERRDEF_H__
+#define __ORION_OKIT_ERRDEF_H__
+
+/* This contains error macros that OKit will use when encountering an error. */
+
+/* GENERIC */
+#define OERR_UNKNOWN							-1	/* Emits when OKit has encountered an error but can't resolve the type.  */
+#define OERR_NONE								0	/* Emits when OKit successfully exits. */
+#define OERR_CANTMALLOC							1	/* Emits when OKit can't malloc() any more memory. */
+
+/* X11 */
+#define OERR_X11_CONNECTION_FAILURE				2	/* Emits when OKit fails to connect with X11. */
+#define OERR_X11_CONNECTION_INTERRUPTED			3	/* Emits when OKit's X11 connection gets interrupted but can't resolve the type. */
+#define OERR_X11_NOT_INITED						4	/* Emits when OKit tries to create an object while X11 is not initialised. */
+#define OERR_X11_WINDOW_CREATION_FAILURE		5	/* Emits when X fails to return a valid Window. */
+
+/* Signals */
+#define OERR_CSIGNALLISTENER_CALLONERROR		6	/* Emits when 'call(void*)' is called on a CSignalListener with 'type' set to CSLT_ERROR. */
+#define OERR_CEVENTARRAY_LINKTONONCHILD			7	/* Emits when 'link(CEvent*,CSignalListener&)' is called on a CEventArray but the 'CEvent*' is NOT owned by the array. */
+#define OERR_CEVENTARRAY_UNLINKTONONCHILD		8	/* Emits when 'unlink(CEvent*,void* func)' is called on a CEventArray but the 'CEvent*' is NOT owned by the array. */
+
+#endif /* !__ORION_OKIT_ERRDEF_H__ */
