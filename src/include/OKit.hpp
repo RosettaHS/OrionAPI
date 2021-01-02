a0674d84b5fe514fb21b36ca73fb518225523dd9
Mark Redmann <markredmann@icloud.com>
2021-01-02 19:35:34 +0800

    Changed formatting in copyright header

--- src/include/OKit.hpp
+++ src/include/OKit.hpp
@@ -3,15 +3,15 @@
 /*                              Copyright (c) 2021                                */
 /*                           Rosetta H&S International                            */
 /*                                                                                */
-/* Permission is hereby granted, free of charge, to any person obtaining a copy   */
-/* of this software and associated documentation files (the "Software"), to deal  */
-/* in the Software without restriction, including without limitation the rights   */
-/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      */
-/* copies of the Software, and to permit persons to whom the Software is          */
-/* furnished to do so, subject to the following conditions:                       */
+/*  Permission is hereby granted, free of charge, to any person obtaining a copy  */
+/*  of this software and associated documentation files (the "Software"), to deal */
+/*  in the Software without restriction, including without limitation the right   */
+/*    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell   */
+/*       copies of the Software, and to permit persons to whom the Software is    */
+/*          furnished to do so, subject to the following conditions:              */
 /*                                                                                */
 /* The above copyright notice and this permission notice shall be included in all */
-/* copies or substantial portions of the Software.                                */
+/*                copies or substantial portions of the Software.                 */
 /*                                                                                */
 /* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     */
 /* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       */
@@ -25,6 +25,10 @@
 
 #ifndef __ORION_OKIT_H__
 #define __ORION_OKIT_H__
+
+#define OKIT_VERSION 	0
+#define OKIT_REVISION	0
+#define OKIT_LICENSE 	"MIT"
 
 #include "errdef.h"
 #include "application.hpp"
