6d14f019e03218fbfa1a243631f60d4029fb6f75
Mark Redmann <markredmann@icloud.com>
2021-01-06 04:28:13 +0800

    Added warning and error condition if CContainer is linked to istelf

--- src/CContainer.cpp
+++ src/CContainer.cpp
@@ -23,6 +23,8 @@
 /*                                                                                */
 /**********************************************************************************/
 
+#include <stdio.h>
+#include "include/application.hpp"
 #include "include/CDrawable.hpp"
 #include "include/CContainer.hpp"
 #include "include/CContainable.hpp"
@@ -38,6 +40,7 @@ namespace Orion{
 		contextToUse=0;
 	}
 	bool CContainer::link(CContainable* obj){
+		if((void*)this==(void*)obj){OVERB_OUT "OKIT | WARNING! CANNOT LINK A CCONTAINER TO ITSELF!\n" OVERB_END return false;}
 		if(children.link(obj)){
 			childCount=children.count;
 			return true;
@@ -46,6 +49,7 @@ namespace Orion{
 	}
 
 	bool CContainer::unlink(CContainable* obj){
+		if((void*)this==(void*)obj){OVERB_OUT "OKIT | WARNING! CANNOT UNLINK A CCONTAINER FROM ITSELF!\n" OVERB_END return false;}
 		if(children.link(obj)){
 			childCount=children.count;
 			return true;
