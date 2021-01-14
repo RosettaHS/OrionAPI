9a88f35e89205aa4a7cb0ff3e07a0d9269134003
Mark Redmann <markredmann@icloud.com>
2021-01-15 06:42:40 +0800

    Added base definitions for OSignals

---
+++ src/include/OSignal.hpp
@@ -0,0 +1,75 @@
+/**********************************************************************************/
+/*                                                                                */
+/*                              Copyright (c) 2021                                */
+/*                           Rosetta H&S International                            */
+/*                                                                                */
+/*  Permission is hereby granted, free of charge, to any person obtaining a copy  */
+/*  of this software and associated documentation files (the "Software"), to deal */
+/*  in the Software without restriction, including without limitation the right   */
+/*    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell   */
+/*       copies of the Software, and to permit persons to whom the Software is    */
+/*          furnished to do so, subject to the following conditions:              */
+/*                                                                                */
+/* The above copyright notice and this permission notice shall be included in all */
+/*                copies or substantial portions of the Software.                 */
+/*                                                                                */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  */
+/* SOFTWARE.                                                                      */
+/*                                                                                */
+/**********************************************************************************/
+
+#ifndef __ORION_OKIT_OSIGNAL_H__
+#define __ORION_OKIT_OSIGNAL_H__
+
+#include <sys/types.h>
+#include "CBaseUI.hpp"
+#include "OString.hpp"
+
+namespace Orion{
+	/* An enumeration of types of Signals that CSignalDispatchers can dispatch. */
+	enum OSignalType{
+		OSIG_NONE,
+		OSIG_BOOL,
+		OSIG_INT,
+		OSIG_UNSIGNEDINT,
+		OSIG_SHORT,
+		OSIG_UNSIGNED_SHORT,
+		OSIG_FLOAT,
+		OSIG_DOUBLE,
+		OSIG_CHAR,
+		OSIG_UNSIGNED_CHAR,
+		OSIG_STRING,
+		OSIG_ARBITRARY,
+		OSIG_OBJECT,
+	};
+	
+	struct OSignal{
+		/* The object that emitted this Signal. */
+		CBaseUI* obj;
+		/* The type of the Signal. */
+		OSignalType type;
+
+		/* The data carried by the Signal. */
+		union{
+			bool asBool;
+			int asInt;
+			unsigned int asUnsignedInt;
+			short asShort;
+			unsigned short asUnsignedShort;
+			float asFloat;
+			double asDouble;
+			char asChar;
+			unsigned char asUnsignedChar;
+			OString asString;
+			void* asArbitrary;
+			CBaseUI* asObject;
+		}get;
+	};
+}
+
+#endif /* !__ORION_OKIT_OSIGNAL_H__ */
