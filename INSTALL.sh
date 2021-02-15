593f1418c15e6baba563260f4999e572ee2441df
Mark Redmann <markredmann@icloud.com>
2021-02-15 08:11:56 +0800

    Added new installer

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,133 +1,130 @@
 #!/bin/sh
 
-# -q for quiet means no ASCII art
-# Both -s or -q could've worked for this flag so I picked -q
-while
-	getopts q option
-do
-	case $option in
-	q) qflag=1;;
-	# When an unrecognised flag is seen, just exit.  We could do a real error/usage message before exit, but I didn't.
-	*) exit 1;;
-	esac
-done
+AUTO_Y=false
+QUIET=false
+PREV_INST=false
+
+### Startup
 
 if [ ! -e "INSTALL.sh" ]; then
-	echo "OrionAPI | : Please run inside the directory this script is located in."
-	exit 1
+	echo "OrionAPI : Please run inside the directory this script is located in."; exit 1
 fi
 
 if [ "$(id -u)" -ne 0 ]; then
-	echo "OrionAPI | : Please run as root."
-	exit 1
+	echo "OrionAPI : Please run as root."; exit 1
 fi
 
-if [ -z "$qflag" ]; then
-	tput clear
-	# Thanks Ron Fritz!
-	echo
-	echo "o          \`O        o                                                     .oOOOo.  \`o    O          "
-	echo "O           o       O                                                     .O     o.  o   O   o       "
-	echo "o           O       o                                     O               O       o  O  O         O  "
-	echo "O           O       O                                    oOo              o       O  oOo         oOo "
-	echo "o     o     o .oOo. o  .oOo  .oOo. \`oOOoOO. .oOo.         o   .oOo.       O       o  o  o    O    o  "
-	echo "O     O     O OooO' O  O     O   o  O  o  o OooO'         O   O   o       o       O  O   O   o    O  "
-	echo "\`o   O o   O' O     o  o     o   O  o  O  O O             o   o   O       \`o     O'  o    o  O    o  "
-	echo "\`OoO' \`OoO'  \`OoO' Oo \`OoO' \`OoO'  O  o  o \`OoO'         \`oO \`OoO'        \`OoooO'   O     O o'   \`oO"
-	echo
-fi
-                                                                                                     
-
-[ -d "/usr/include/Orion" ] || mkdir "/usr/include/Orion"
-
-if [ -e "/usr/lib/libOKit.so" ]; then
-	printf "Orion | OKit is already installed, would you like to re-install? (y/N) : "
-	read conf
-	case ${conf} in
-		[!Yy]) echo "Orion | OKit : Aborted."
-			exit 1;;
+while getopts qy flag; do
+	case $flag in
+	q) QUIET=true;;
+	y) AUTO_Y=true;;
+	*) echo "OrionAPI | ERROR! : Invalid Operand. Aborting"; exit 1;;
 	esac
-	# There's no error checking here in the next three rm commands...?  Just checking to see if it's intentional or not.  I don't think error-checking would be 100% necessary as you'll install over these files anyway.  Still would be nice, though.
-	rm -rf "/usr/include/Orion/_OKit"
-	rm "/usr/include/Orion/OKit"
-	rm "/usr/lib/libOKit.so"
-	echo "Orion | OKit : Reinstalling OKit."
+done
+
+### Execution
+
+if [ "$QUIET" = false ]; then
+	clear
+	echo "o          \`O        o                                                     .oOOOo.                            Oo    OooOOo.  ooOoOOo       oO"
+	echo "O           o       O                                                     .O     o.        o                 o  O   O     \`O    O          OO"
+	echo "o           O       o                                     O               O       o                         O    o  o      O    o          oO"
+	echo "O           O       O                                    oOo              o       O                        oOooOoOo O     .o    O          Oo"
+	echo "o     o     o .oOo. o  .oOo  .oOo. \`oOOoOO. .oOo.         o   .oOo.       O       o \`OoOo. O  .oOo. 'OoOo. o      O oOooOO'     o          oO"
+	echo "O     O     O OooO' O  O     O   o  O  o  o OooO'         O   O   o       o       O  o     o  O   o  o   O O      o o           O            "
+	echo "\`o   O o   O' O     o  o     o   O  o  O  O O             o   o   O       \`o     O'  O     O  o   O  O   o o      O O           O          Oo"
+ 	echo " \`OoO' \`OoO'  \`OoO' Oo \`OoO' \`OoO'  O  o  o \`OoO'         \`oO \`OoO'        \`OoooO'   o     o' \`OoO'  o   O O.     O o'       ooOOoOo       oO"
+	# Add more here later!
 fi
 
-# Instead of using dpkg for this test, you should see if a test like
-# command -v make
-# works or not.
+echo ""
+
+# Check if system has "make", and if not, install it.
 if ! dpkg -s "make" >/dev/null 2>&1; then 
-	printf 'Orion | OKit : "make" is not installed. It is required for OKit to be compiled. Would you like to install this dependency? (y/N) : '
-	read confb
-	case ${confb} in
-		[!Yy]) echo 'Orion | OKit : Aborted.'
-			exit 1;;
-	esac
-	echo "Orion | OKit : Working on installing Make."
+	if [ "$AUTO_Y" = true ]; then
+		echo 'OrionAPI : Dependency "make" is not installed. Retrieving.'
+	else
+		printf 'OrionAPI  : Dependency "make" is not installed. It is required for OrionAPI to be compiled. Would you like to install this dependency? [Y/n] : '; read conf
+		case ${conf} in
+			[!Yy]) echo 'OrionAPI : Aborted.'; exit 1;;
+		esac
+	fi
+
+	echo "OrionAPI : Working on installing Make."
 	if apt-get install "make" -y; then
-		echo "Orion | OKit : Make successfully installed."
+		echo "OrionAPI : Make successfully installed."
 	else
-		echo "Orion | OKit : ERROR! Make could not be installed! Aborting."
-		exit 1
+		echo "OrionAPI | ERROR! : Make could not be installed! Aborting."; exit 1
 	fi
 fi
 
+# Check if system has "libx11-dev", and if not, install it.
 if ! dpkg -s "libx11-dev" >/dev/null 2>&1; then 
-	printf 'Orion | OKit : "libx11-dev" is not installed. It is required for OKit to function. Would you like to install this dependency? (y/N) : '
-	read confc
-	case ${confc} in
-		[!Yy]) echo 'Orion | OKit : Aborted.'
-			exit 1;;
-	esac
-	echo "Orion | OKit : Working on installing XLib."
-	if apt-get install "libx11-dev" -y
-	then
-		echo "Orion | OKit : XLib successfully installed."
+	if [ "$AUTO_Y" = true ]; then
+		echo 'OrionAPI : Dependency "libx11-dev" is not installed. Retrieving.'
 	else
-		echo "Orion | OKit : ERROR! XLib could not be installed! Aborting."
-		exit 1
+		printf 'OrionAPI  : Dependency "libx11-dev" is not installed. It is required for OrionAPI to be compiled. Would you like to install this dependency? [Y/n] : '; read conf
+		case ${conf} in
+			[!Yy]) echo "OrionAPI : Aborted."; exit 1;;
+		esac
+	fi
+
+	echo "OrionAPI : Working on installing Xlib."
+	if apt-get install "libx11-dev" -y; then
+		echo "OrionAPI : Xlib successfully installed."
+	else
+		echo "OrionAPI | ERROR! : Xlib could not be installed! Aborting."; exit 1
 	fi
 fi
 
-printf 'Orion | OKit : OKit is ready to be installed. Would you like to proceed? (y/N) : '; read confd
-case ${confd} in
-	[!Yy]) echo 'Orion | OKit : Aborted.'
-		exit 1;;
-esac
+# Check if OrionAPI currently exists on this system, and if it does, remove the current version. 
+if [ -e "/usr/lib/libO.so" ]; then
+	PREV_INST=true
+	if [ "$AUTO_Y" = true ]; then
+		echo "OrionAPI : OrionAPI is already installed, removing currently installed version for reinstall."
+	else
+		printf "OrionAPI : OrionAPI is already installed, would you like to reinstall? [Y/n] : "; read conf
+		case ${conf} in
+			[!Yy]) echo "OrionAPI : Aborted."; exit 1;;
+		esac
 
-echo "Orion | OKit : Working on installing OKit."
-if ! cp -R "src/include/" "/usr/include/Orion/_OKit"; then
-	echo 'Orion | OKit : ERROR! Failed to copy header files into "/usr/include/Orion/_OKit"! Aborting.'
-	exit 1
+		rm -rf "/usr/include/.OrionAPI" >/dev/null 2>&1 || echo 'OrionAPI | WARNING! : Current version include folder "/usr/include/.OrionAPI" not found, failed to remove.'
+		rm "/usr/include/OrionAPI" >/dev/null 2>&1		|| echo 'OrionAPI | WARNING! : Current version header file "/usr/include/OrionAPI" not found, failed to remove.'
+		rm "/usr/lib/libO.so" >/dev/null 2>&1			|| echo 'OrionAPI | WARNING! : Current version source library "/usr/lib/libO.so" not found, failed to remove.'
+	fi
+fi
+echo "OrionAPI : Current version successfully removed. "
+
+# Ready to be installed. If flag "-y" is not set, ask for permisson to proceed.
+if [ "$AUTO_Y" = false -a "$PREV_INST" = false ]; then
+	printf 'OrionAPI : OrionAPI is ready to be installed. Would you like to proceed? [Y/n] : '; read conf
+	case ${conf} in
+		[!Yy]) echo 'OrionAPI : Aborted.'; exit 1;;
+	esac
 fi
 
-if cp "src/SYSHEADER" "/usr/include/Orion/OKit"; then
-	echo 'Orion | OKit : Copied header files. Use "#include <Orion/OKit>" in your C++ files to use these headers.'
-else
-	echo 'Orion | OKit : ERROR! Failed to copy redirect header as "/usr/include/Orion/OKit"! Aborting.'
-	exit 1
-fi
+# Compiles the source code.
+echo "OrionAPI : Compiling source code..."
+make shared >/dev/null 2>&1 || { echo "OrionAPI : ERROR! Failed to compile source code! Aborting."; exit 1; }
+echo "OrionAPI : Successfully compiled OKit source code."
 
-echo "Orion | OKit : Compiling source code..."
+# Moves it to the "/usr/lib" directory.
+cp "libO.so" "/usr/lib/libO.so" || { echo 'OrionAPI : ERROR! Failed to copy compiled library as "/usr/lib/libO.so"! Aborting.'; exit 1; }
+echo "OrionAPI : Copied OrionAPI (libO.so) to system libaries directory."
 
-if make shared >>/dev/null; then
-	echo "Orion | OKit : Successfully compiled OKit source code."
-else
-	echo "Orion | OKit : ERROR! Failed to compile source code! Aborting."
-	exit 1
-fi
-
-if cp "libOKit.so" "/usr/lib/libOKit.so"; then
-	echo "Orion | OKit : Copied OKit library to system libaries directory."
-else
-	echo 'Orion | OKit : ERROR! Failed to copy compiled library as "/usr/lib/libOKit.so"! Aborting.'
-	exit 1
-fi
-
+# Removes the compiled files from the current directory.
 rm ./*.o ./*.so >/dev/null 2>&1
 
-if [ -z "$qflag" ]; then
+# Installs the header files from "src/include" into "/usr/include/.OrionAPI".
+echo "OrionAPI : Working on installing header files..."
+cp -R "src/include/" "/usr/include/.OrionAPI" || { echo 'OrionAPI : ERROR! Failed to copy header files into "/usr/include/.OrionAPI"! Aborting.'; exit 1; }
+
+# Installs the redirect header "src/SYSHEADER" as "usr/include/OrionAPI".
+cp "src/SYSHEADER" "/usr/include/OrionAPI" || { echo 'OrionAPI : ERROR! Failed to copy redirect header as "/usr/include/OrionAPI"! Aborting.'; exit 1; }
+echo 'OrionAPI : Copied header files. Use "#include <OrionAPI>" in your C++ files to use these headers.'
+
+# End ASCII
+if [ "$QUIET" = false ]; then
 	echo
 	echo ".oOOOo.                                             oO"
 	echo "o     o                                             OO"
@@ -140,5 +137,4 @@ if [ -z "$qflag" ]; then
 	echo
 fi
 
-echo
-echo 'Orion | OKit : OKit has been successfully installed. To compile your applications with OKit, simply use the library flag "-lOKit", and read the files in "docs" for more information.'
+printf "\nOrionAPI : OrionAPI has been successfully installed. To compile your applications with the OrionAPI, simply use the library flag \"-lO\", and read the files in \"docs\" for more information.\n\n"
