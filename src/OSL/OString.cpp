89a3f67b023994081b56adecdc3786d68e90592d
Mark Redmann <markredmann@icloud.com>
2021-03-17 12:22:21 +0800

    Added primitive full-width UTF8 individual character support

--- src/OSL/OString.cpp
+++ src/OSL/OString.cpp
@@ -30,9 +30,68 @@
 #include "../include/OSL/OString.hpp"
 #include "../include/OSL/OLog.hpp"
 
-namespace Orion{
+#define BITCOUNT 8
 
-/* Generic String functions. */
+MAXCONST unsigned char bits[BITCOUNT]={0x80,0x40,0x20,0x10,0x8,0x4,0x2,0x1};
+
+namespace Orion{
+/*** Characters ***/
+
+	OChar::OChar(const char* multibyte)            { setTo(multibyte);  }
+	OChar::OChar(char singlebyte)                  { setTo(singlebyte); }
+	OChar& OChar::operator=(const char* multibyte) { setTo(multibyte);  return *this; }
+	OChar& OChar::operator=(char singlebyte)       { setTo(singlebyte); return *this; }
+
+	void OChar::setTo(const char* multibyte){
+		clear();
+		for(unsigned char i=0;i<OCHARBYTES-1;i++){
+			unsigned char c=multibyte[i];
+			if(c!=0){
+				get.asMultiByte[i]=c;
+				unsigned char uniflags=0;
+				while(1){ if(c&bits[uniflags]){ uniflags++; }else{ break; } }
+				bd[i]=( (uniflags<=6) ? (OUnicodeType)uniflags : OUNI_UNKNOWN );
+				byteCount++;
+				/* Only store the first character of a multi-byte "character"  if it's not actually UTF-8. */
+				if(uniflags==OUNI_ASCII && byteCount<=1){ isUnicode=false; break; }
+				else                                    { isUnicode=true; }
+			}else{ bd[i]=OUNI_NULL; break; }
+		}
+	}
+
+	void OChar::log(bool verbose){
+		if(verbose){
+			for(unsigned char i=0;i<OCHARBYTES;i++){
+				OLog("[ ");
+				for(unsigned char j=0;j<BITCOUNT;j++){
+					( ((get.asMultiByte[i])&bits[j]) ? OLog("1") : OLog("0") );
+				}
+				switch(bd[i]){
+					default:            { OLog(" | ERROR "); break; }
+					case OUNI_NULL:     { OLog(" | UCBD_NULL "); break; }
+					case OUNI_ASCII:    { OLog(" | UCBD_ASCII "); break; }
+					case OUNI_HEADER_1X:{ OLog(" | UCBD_HEADER_1X "); break; }
+					case OUNI_HEADER_2X:{ OLog(" | UCBD_HEADER_2X "); break; }
+					case OUNI_HEADER_3X:{ OLog(" | UCBD_HEADER_3X "); break; }
+					case OUNI_HEADER_4X:{ OLog(" | UCBD_HEADER_4X "); break; }
+					case OUNI_HEADER_5X:{ OLog(" | UCBD_HEADER_5X "); break; }
+					case OUNI_CONTINUE: { OLog(" | UCBD_CONT "); break; }
+					case OUNI_UNKNOWN:  { OLog(" | UCBD_CONT "); break; }
+				}
+				OLog("]\n");
+			}
+		}else{
+			OLog("%s\n",get.asMultiByte);
+		}
+	}
+
+/** Generic Character functions **/
+
+/* TODO: Add them. */
+
+/*** Strings ***/
+
+/** Generic String functions. **/
 	void OFormat(char* output, const char* format, ...){
 		if(!output){ OLog("ORIONAPI | WARNING! CANNOT PASS NULL WHEN FORMATTING A STRING TO A BUFFER!\n"); return; }
 		va_list arg;
--- src/include/OSL/OString.hpp
+++ src/include/OSL/OString.hpp
@@ -27,10 +27,81 @@
 #define __ORIONAPI_OSL_OSTRING_H__
 
 #include "common.hpp"
+#include "OLog.hpp"
 
+/*** UTF8 char #defines ***/
+
+#define OCHARBYTES 5
+
+#define CHARBIT_UTF8_MB (0x80)
+#define CHARBIT_UTF8_1X (0x40)
+#define CHARBIT_UTF8_2X (0x20)
+#define CHARBIT_UTF8_3X (0x10)
+#define CHARBIT_UTF8_4X (0x8)
+#define CHARBIT_UTF8_5X (0x4)
+#define CHARBIT_UTF8_6X (0x2) /* Invalid! Only used for calculation. */
+
+/*** String #defines *///
+
+/* If any String lookup function returns this, it means the lookup failed. */
 #define OSTRING_NOTFOUND ULONG_MAX
 
 namespace Orion{
+/*** Characters ***/
+	/* An enumeration of UTF8 states for a given byte. */
+	enum OUnicodeType : char{
+		/* The byte is comprised entirely of zeros; a NULL terminator. */
+		OUNI_NULL=-1,
+		/* The byte is ASCII, and should not be treated as unicode. */
+		OUNI_ASCII,
+		/* The byte is a UTF8 continuation byte. */
+		OUNI_CONTINUE,
+		/* The byte is a UTF8 header byte that calls for one extra continuation byte. */
+		OUNI_HEADER_1X,
+		/* The byte is a UTF8 header byte that calls for two extra continuation bytes. */
+		OUNI_HEADER_2X,
+		/* The byte is a UTF8 header byte that calls for three extra continuation bytes. */
+		OUNI_HEADER_3X,
+		/* The byte is a UTF8 header byte that calls for four extra continuation bytes. */
+		OUNI_HEADER_4X,
+		/* The byte is a UTF8 header byte that calls for five extra continuation bytes. */
+		OUNI_HEADER_5X,
+		/* The byte is of an unknown type. */
+		OUNI_UNKNOWN
+	};
+	/* An Orion-Native wrapper for individual Unicode characters. */
+	struct OChar : CLoggable{
+		/* Choose which way you wish to interpret the character data. */
+		union{
+			/* Gets the character data as single-byte (first index), like an ordinary character. Only for non-Unicode Chars. */
+			char          asSingleByte;
+			/* Gets the character data as a multi-byte, like a string. Only for Unicode Chars. */
+			unsigned char asMultiByte[OCHARBYTES];
+		}get;
+		OUnicodeType  bd[OCHARBYTES];
+		unsigned char byteCount;
+		bool          isUnicode;
+
+		/* Empty constructor. Sets all values to 0. */
+		inline OChar(void) : get{0}, bd{OUNI_NULL,OUNI_NULL,OUNI_NULL,OUNI_NULL,OUNI_NULL}, byteCount{0}, isUnicode{0} {}
+		/* Sets this Char's contents to the given multi-byte character [string]. */
+		OChar(const char*); void setTo(const char*); OChar& operator=(const char*);
+		/* Sets this Char's contents to the given single-byte character. */
+		OChar(char); void setTo(char); OChar& operator=(char);
+
+		/* Clears this Char's contents. */
+		inline void clear(void){ byteCount=0; isUnicode=0; for(unsigned char i=0;i<OCHARBYTES;i++){ get.asMultiByte[i]=0; bd[i]=OUNI_NULL; } }
+
+		/* Converts this Char into a standard single-byte character, or the first byte of the multi-byte character if this is Unicode. */
+		inline operator char(void)       { return get.asSingleByte; }
+		/* Converts this Char into a String, or nothing if this is not Unicode. */
+		inline operator const char*(void){ return ( (isUnicode) ? (const char*)get.asMultiByte : 0 ); }
+
+		/* Logs this Char's contents out to the terminal. Pass true to get byte and bit information. */
+		virtual void log(bool verbose=false) override;
+	};
+
+/*** Strings ***/
 	/* Writes the formatted String to the output. */
 	extern void OFormat(char* output, const char* format, ...);
 	/* Returns the length of the passed String. */
