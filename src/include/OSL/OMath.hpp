d89b9dc54aefd13d02e3fbc66785cda2cae7e322
Mark Redmann <markredmann@icloud.com>
2021-03-26 23:43:07 +0800

    Updated tooltips for math functions

--- src/include/OSL/OMath.hpp
+++ src/include/OSL/OMath.hpp
@@ -29,12 +29,30 @@
 #include "common.hpp"
 
 namespace Orion{
-	/* Returns the given value "x" if it is greater than "min", but less than "max". Otherwise returns "min" if smaller, or "max" if greater. */
+	/**
+	 * @brief Returns a version of the given value clamped between the two other values.
+	 * @param x The value to clamp.
+	 * @param min The minimum for the value.
+	 * @param max The maximum for the value.
+	 * @return If the given value "x" is within the range between "min" and "max", it will be returned as normal.
+	 * Otherwise, the minimum or maximum value will be returned depending on which is the closest value to "x".
+	 */
 	inline long OClamp(long& x, long& min, long& max){ return ( (x<min) ? (min) : ( (x>max) ? max : x) ); }
-
-	/* Returns the given value "x" if it is less than "max", otherwise returns "max". */
+	/**
+	 * @brief Returns a version of the given value clamped below the second value.
+	 * @param x The value to clamp.
+	 * @param max The maximum for the value.
+	 * @return If the given value "x" is less than the maximum value, the value of "x" will be returned.
+	 * Otherwise, the maximum value will be returned.
+	 */
 	inline long OClampMax(long& x, long& max){ return ( (x>max) ? max : x ); }
-	/* Returns the given value "x" if it is greater than "min", otherwise returns "min". */
+	/**
+	 * @brief Returns a version of the given value clamped above the second value.
+	 * @param x The value to clamp.
+	 * @param min The minimum for the value
+	 * @return If the given value "x" is greater than the minimum value, the value of "x" will be returned.
+	 * Otherwise, the minimum value will be returned.
+	 */
 	inline long OClampMin(long& x, long& min){ return ( (x<min) ? min : x ); }
 }
 
