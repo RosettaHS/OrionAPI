d3e8772b3d633dadd312357d72405b60dbceda5c
rosettahs <markredmann@icloud.com>
2021-05-10 08:23:12 +0800

    Added tooltips for OContainer

--- src/include/OKit/OContainer.hpp
+++ src/include/OKit/OContainer.hpp
@@ -126,45 +126,108 @@ namespace Orion{
 	/* The base Container for containing OrionUI Elements. */
 	class OContainer : public OWidget{
 		protected:
+			/* The internal list of child Widgets. */
 			CWidgetList list;
+			/* The Context used to display this Container (and its children) to the screen. */
 			CContext    selfContext;
+			/* The Context for linked Widgets to use. */
 			CContext*   contextToUse;
+			/*
+			 * The Container for linked Widgets to use.
+			 * Some derived Containers may consist of several nested Containers.
+			 */
 			OContainer* containerToUse;
+			/* Should this Container force itself to be used next time link() is called, instead of containerToUse? */
 			bool        forceSelfOnNext;
+			/* Has this Container been unlinked from its parent, and is awaiting relinkage? */
 			bool        holdingInTmp;
 
+			/* Empty constructor. Sets all values to 0. */
 			OContainer(void);
 
+			/* The base sort used by OContainer. Does nothing, and lets the Widgets sort themselves with flags. */
 			void             baseSort(void);
+			/* Calls when this Container gets linked to a new parent. */
 			virtual void     onLink(void)         override;
+			/* Calls when this Container gets unlinked from its parent. */
 			virtual void     onUnlink(void)       override;
+			/* Calls when this Container gets its position modified. */
 			virtual void     onPosChanged(void)   override;
+			/* Calls when this Container gets its size modified. */
 			virtual void     onSizeChanged(void)  override;
+			/* Calls when this Container gets its theme modified. */
 			virtual void     onColChanged(void)   override;
+			/* Temporarily unlinks all Widgets when this Container gets unlinked and stores them for later when it gets relinked. */
 			void             tmpUnlinkAll(void);
+			/* Relinks all temporarily held Widgets once this Container gets linked to a new parent. */
 			void             tmpRelinkAll(void);
 		public:
+			/* Destructor. Unlinks all Widgets and frees all memory. */
 			virtual ~OContainer(void);
+			/**
+			 * @brief Creates and links an OContainer to the parent Container with the given parameters.
+			 * @param parent The Container to link this Container to.
+			 * @param x The X position relative to the parent Container.
+			 * @param y The Y position relative to the parent Container.
+			 * @param w The width of this Container
+			 * @param h The height of this Container
+			 */
 			OContainer(OContainer* parent, int16_t x, int16_t y, uint16_t w, uint16_t h);
 
-			bool             link(OWidget* widget);
-			inline bool      link(OWidget& widget)   { return link(&widget); }
-			bool             unlink(OWidget* widget);
-			inline bool      unlink(OWidget& widget) { return unlink(&widget); }
-			bool             unlinkAll(void);
+			/**
+			 * @brief Links a Widget to this Container.
+			 * @param widget The Widget (pointer/reference) to link to this Container.
+			 * @return True if linkage was successful, otherwise false.
+			 */
+			bool link(OWidget* widget); inline bool link(OWidget& widget)     { return link(&widget); }
+			/**
+			 * @brief Unlinks a Widget from this Container.
+			 * @param widget The Widget (pointer/reference) to unlink from this Container.
+			 * @return True if unlinkage was successful, otherwise false if Widget could not be unlinked, or Widget is not linked to this Container.
+			 */
+			bool unlink(OWidget* widget); inline bool unlink(OWidget& widget) { return unlink(&widget); }
+			/**
+			 * @brief Unlinks all Widgets from this Container.
+			 * @return True if unlinkages were successful, otherwise false.
+			 */
+			bool unlinkAll(void);
 
-			virtual void     sort(void);
+			/*
+			 * Sorts the child Widgets of this Container.
+			 * Automatically called whenever Container is resized, or new Widgets are added.
+			 */
+			virtual void sort(void);
 
-			virtual void     setCol(uint8_t r, uint8_t g, uint8_t b) override;
-			virtual void     setCol(OCol* col)                       override;
-			virtual void     setCol(OCol& col)                       override;
+			/**
+			 * @brief Sets the Colour of this Container to the given RGB values.
+			 * @param r The red value of the Colour to set.
+			 * @param g The greenvalue of the Colour to set.
+			 * @param b The blue value of the Colour to set.
+			 */
+			virtual void setCol(uint8_t r, uint8_t g, uint8_t b) override;
+			/**
+			 * @brief Sets the Colour of this Container to the given OCol.
+			 * @param col The OCol (pointer/reference) to set as this Container's background.
+			 */			
+			virtual void setCol(OCol* col) override; virtual void setCol(OCol& col) override;
 
-			int32_t          getIndexOf(const OWidget* widget) const;
-			inline int32_t   getIndexOf(const OWidget& widget) const { return getIndexOf(&widget); }
-			OWidget*         getChild(uint16_t index)    const;
-			inline OWidget*  operator[](uint16_t index)  const { return getChild(index); }
-			OWidget**        getChildren(void)           const;
-			uint16_t         getChildCount(void)         const;
+			/**
+			 * @brief Returns the index of the given Widget if it is linked to this Container.
+			 * Note: The index can change at any moment if new Widgets are added or removed, therefore it is unsafe to store this value and never update it.
+			 * @param widget The Widget (pointer/reference) to attempt to scan and retrieve the index of.
+			 * @return If the Widget was found, its index is returned. Otherwise, returns OWIDGET_NOTLINKED.
+			 */
+			int32_t  getIndexOf(const OWidget* widget) const; inline int32_t getIndexOf(const OWidget& widget) const { return getIndexOf(&widget); }
+			/**
+			 * @brief Returns the child Widget at the given index.
+			 * @param index The index to attempt to retrieve the Widget from
+			 * @return A pointer to the Widget if it could be found, otherwise returns NULL.
+			 */
+			OWidget*   getChild(uint16_t index)    const; inline OWidget* operator[](uint16_t index)  const { return getChild(index); }
+			/* Returns the array of Widget pointers used by this Container. */
+			OWidget**  getChildren(void)           const;
+			/* Returns the count of Widgets linked to this Container. */
+			uint16_t   getChildCount(void)         const;
 	};
 
 	#ifndef ORION_NOALTNAMES
